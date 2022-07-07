3286d20ffa9b126a7093c502249b44340ecc3de1
bartlomiej.filipiuk <bartlomiej.filipiuk@droptica.pl>
2022-07-07 13:02:19 +0200

    platformSH

--- web/sites/default/settings.platformsh.php
+++ web/sites/default/settings.platformsh.php
@@ -126,32 +126,32 @@ if (isset($_ENV['PLATFORM_ROUTES']) && !isset($settings['trusted_host_patterns']
 // - d8settings:ensighten
 // - d8settings:f1_endpoint_nl
 // - d8settings:f1_endpoint_fr
-if (isset($_ENV['PLATFORM_VARIABLES'])) {
-  $variables = json_decode(base64_decode($_ENV['PLATFORM_VARIABLES']), TRUE);
-  foreach ($variables as $name => $value) {
-    // A variable named "d8settings:example-setting" will be saved in
-    // $settings['example-setting'].
-    if (strpos($name, 'd8settings:') === 0) {
-      $settings[substr($name, 11)] = $value;
-    }
-    // A variable named "drupal:example-setting" will be saved in
-    // $settings['example-setting'] (backwards compatibility).
-    elseif (strpos($name, 'drupal:') === 0) {
-      $settings[substr($name, 7)] = $value;
-    }
-    // A variable named "d8config:example-name:example-key" will be saved in
-    // $config['example-name']['example-key'].
-    elseif (strpos($name, 'd8config:') === 0 && substr_count($name, ':') >= 2) {
-      list(, $config_key, $config_name) = explode(':', $name, 3);
-      $config[$config_key][$config_name] = $value;
-    }
-    // A complex variable named "d8config:example-name" will be saved in
-    // $config['example-name'].
-    elseif (strpos($name, 'd8config:') === 0 && is_array($value)) {
-      $config[substr($name, 9)] = $value;
-    }
-  }
-}
+//if (isset($_ENV['PLATFORM_VARIABLES'])) {
+//  $variables = json_decode(base64_decode($_ENV['PLATFORM_VARIABLES']), TRUE);
+//  foreach ($variables as $name => $value) {
+//    // A variable named "d8settings:example-setting" will be saved in
+//    // $settings['example-setting'].
+//    if (strpos($name, 'd8settings:') === 0) {
+//      $settings[substr($name, 11)] = $value;
+//    }
+//    // A variable named "drupal:example-setting" will be saved in
+//    // $settings['example-setting'] (backwards compatibility).
+//    elseif (strpos($name, 'drupal:') === 0) {
+//      $settings[substr($name, 7)] = $value;
+//    }
+//    // A variable named "d8config:example-name:example-key" will be saved in
+//    // $config['example-name']['example-key'].
+//    elseif (strpos($name, 'd8config:') === 0 && substr_count($name, ':') >= 2) {
+//      list(, $config_key, $config_name) = explode(':', $name, 3);
+//      $config[$config_key][$config_name] = $value;
+//    }
+//    // A complex variable named "d8config:example-name" will be saved in
+//    // $config['example-name'].
+//    elseif (strpos($name, 'd8config:') === 0 && is_array($value)) {
+//      $config[substr($name, 9)] = $value;
+//    }
+//  }
+//}
 
 // Set the project-specific entropy value, used for generating one-time
 // keys and such.
